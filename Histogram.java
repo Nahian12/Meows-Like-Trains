0490da0816bb21dbb59a1d05b8f4d284f57bfc3b
Saber <70017655+Saber-97@users.noreply.github.com>
2022-06-13 14:09:46 +0500

    Update Histogram.java

--- Histogram.java
+++ Histogram.java
@@ -1,100 +1,103 @@
-import java.io.*;
-import java.util.Scanner;
 import java.util.*;
 import java.util.ArrayList;
 public class Histogram {
 
-        public static void main(String[] args)  throws FileNotFoundException, IOException{
+    public static void main(String[] args) {
 
-            BufferedReader reader = new BufferedReader(new FileReader("0.txt"));
-            String text = reader.readLine();
+        Scanner in = new Scanner(System.in);
+        int cases = in.nextInt();
+        for (int a = 0 ; a < cases ; a++){
 
-            String parts[] = text.split(" ");
-            int amount = Integer.parseInt(parts[0]);
-            int intervals = Integer.parseInt(parts[1]);
+            int amount = in.nextInt();
+            int intervals = in.nextInt();
+            int max = 0;
+            int min = 0;
 
-            LinkedList<String>[] lists = new LinkedList[intervals];
-            for(int i = 0 ; i < intervals;i++) {
-                lists[i] = new LinkedList<>();
+
+            ArrayList<Integer> temp = new ArrayList<>(amount);
+            for (int i = 0 ; i < amount; i++){
+                int value = in.nextInt();
+                temp.add(value);
+
+                if (i == 0) {
+                    max = min = value;
+                }
+
+                if (value > max) {
+                    max =  value;
+                }
+                if (temp.get(i) < min) {
+                    min = value;
+                }
             }
-            String st = null;
-            st = reader.readLine();
-
-            int max=0;
-            int min=0;
-
-            if(st!= null){
-                ArrayList<Integer> number = new ArrayList<>();
-                String temp[] = st.split(" ");
-
-
-
-                for(int i=0 ; i < temp.length ; i++){
-                    if(i ==0){
-                        max = min = Integer.parseInt(temp[i]);
-                    }
-
-                    if (Integer.parseInt(temp[i])> max)
-                    {
-                        max = Integer.parseInt(temp[i]);
-                    }
-                    if (Integer.parseInt(temp[i])< min)
-                    {
-                        min = Integer.parseInt(temp[i]);
-                    }
-                }
-
-                ArrayList<Integer> cutoffs = new ArrayList<>();
-                int middleValueOfCutOffs = (max-min) / intervals;
-                for(int i  = 0 ; i <= intervals ; i++){
-                    int cutOffValue = min + middleValueOfCutOffs*i;
-                    cutoffs.add(cutOffValue);
-
-                }
-
-
-                for(int i = 0 ; i < temp.length; i++){
-                    innerLoop:
-                    for(int j = 0 ; j < intervals; j++){
-                       if(Integer.parseInt(temp[i]) < cutoffs.get(j+1)){
-                           lists[j].add(temp[i]);
-                           break innerLoop;
-
-                       }
-                        if(Integer.parseInt(temp[i]) == max){
-                            lists[intervals-1].add(temp[i]);
-
-                            break innerLoop;
-                        }
-                    }
-                }
-
-
-
-
-
-
-                
-                for(int i  = 0 ; i <= intervals ; i++){
-                    System.out.print(cutoffs.get(i) + " ");
-
-                }
-                System.out.println();
-                for(int i  = 0 ; i < intervals ; i++){
-                    System.out.print(lists[i].size() + " ");
-
-                }
-
-
-
-
-
-            }
-            else{
-                return;
+            ArrayList<Integer>[] lists = new ArrayList[intervals];
+            for (int i = 0; i < intervals; i++) {
+                lists[i] = new ArrayList<>();
             }
 
 
+            ArrayList<Integer> cutoffs = new ArrayList<>();
+            int middleValueOfCutOffs = (max - min) / intervals;
+            for (int i = 0; i <= intervals; i++) {
+                int cutOffValue = min + middleValueOfCutOffs * i;
+                cutoffs.add(cutOffValue);
+
+            }
+
+
+
+            for(int i = 0; i < temp.size(); i++){
+                int index = 0;
+                int indexIdentifier = binarySearch(cutoffs, temp.get(i) );
+                if(indexIdentifier < 0 ){
+                    index = Math.abs(indexIdentifier) - 1;
+                }
+                else if(indexIdentifier >= intervals) {
+                    index = indexIdentifier - 1;
+                }else{
+                    index = indexIdentifier;
+                }
+
+                lists[index].add(temp.get(i));
+            }
+            
+
+
+            for (int i = 0; i <= intervals; i++) {
+                System.out.print(cutoffs.get(i) + " ");
+
+            }
+            System.out.println();
+
+            for (int i = 0; i < intervals; i++) {
+                System.out.print(lists[i].size() + " ");
+
+            }
+            System.out.println();
+
+
+
+
+
+        }
+    }
+    /** Use binary search to find the key in the list */
+    public static int binarySearch(ArrayList<Integer> list, int key) {
+        int low = 0;
+        int high = list.size() - 1;
+
+        while (high >= low) {
+            int mid = (low + high) / 2;
+            if (key < list.get(mid))
+                high = mid - 1;
+            else if (key == list.get(mid))
+                return mid;
+            else
+                low = mid + 1;
+        }
+
+        return -low; // Now high < low
     }
 
 }
+
